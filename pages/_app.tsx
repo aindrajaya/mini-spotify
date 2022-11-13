fcc49afd92466add938f7657a9a473d35aa56e3d
Arista Indrajaya <arista.indrajaya@gmail.com>
2022-11-14 05:25:00 +0700

    chakra ui config
    adding the theme and components button variants

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,44 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import {ChakraProvider, extendTheme} from "@chakra-ui/react";
+import "reset-css"
+import type { AppProps } from "next/app";
+import PlayerLayout from "../components/PlayerLayout";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const theme = extendTheme({
+  colors: {
+    gray: {
+      100: '#F5f5f5',
+      200: '#eeeeee',
+      300: '#e0e0e0',
+      400: '#bdbdbd',
+      500: '#9e9e9e',
+      600: '#757575',
+      700: '#616161',
+      800: '#424242',
+      900: '#212121',
+    },
+  },
+  components: {
+    Button: {
+      variants: {
+        link: {
+          ":focus": {
+            outline: "none",
+            boxShadow: "none"
+          },
+        },
+      },
+    }
+  }
+})
+
+const App = ({ Component, pageProps }: AppProps) => {
+  return(
+    <ChakraProvider theme={theme}>
+      <PlayerLayout>
+        <Component {...pageProps} />
+      </PlayerLayout>
+    </ChakraProvider>
+  )
+};
+
+export default App;
