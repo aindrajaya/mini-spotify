5aa1d6b962895fc532c1ef912e81033e31aa5aa7
Arista Indrajaya <arista.indrajaya@gmail.com>
2022-11-15 05:26:00 +0700

    implement sidebar logo

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,10 +1,22 @@
+import NextImage from "next/image";
 import { Box, List, ListItem, ListIcon, Divider, Center, LinkBox, LinkOverlay} from "@chakra-ui/layout";
 import {MdHome, MdSearch, MdLibraryMusic, MdPlaylistAdd, MdFavorite} from "react-icons/md";
 
 const Sidebar = () => {
   return (
-    <Box>
-      Sidebar
+    <Box
+      width="100%"
+      height="calc(100vh - 100px)"
+      bg="black"
+      paddingX="5px"
+      color="gray"
+    >
+      <Box paddingY="20px">
+        <Box width="120px" marginBottom="20px" paddingY="20px">
+          <NextImage src="/logo.svg" height={60} width={120}/>
+        </Box>
+        Sidebar Menu
+      </Box>
     </Box>
   )
 }
