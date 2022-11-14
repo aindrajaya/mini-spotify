5307760895c9d8a1e0050cbba85f4c7a46847405
Arista Indrajaya <arista.indrajaya@gmail.com>
2022-11-15 05:59:00 +0700

    Implement sidebar menu list

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,4 +1,5 @@
 import NextImage from "next/image";
+import NextLink from "next/link";
 import { Box, List, ListItem, ListIcon, Divider, Center, LinkBox, LinkOverlay} from "@chakra-ui/layout";
 import {MdHome, MdSearch, MdLibraryMusic, MdPlaylistAdd, MdFavorite} from "react-icons/md";
 
@@ -35,7 +36,22 @@ const Sidebar = () => {
         </Box>
         <Box marginBottom="20px">
           <List spacing={2}>
-          Sidebar Layout Menu
+            {NavMenu.map((menu) => (
+              <ListItem paddingX="20px" fontSize="16px" key={menu.name}>
+                <LinkBox>
+                  <NextLink href={menu.route} passHref>
+                    <LinkOverlay>
+                      <ListIcon 
+                        as={menu.icon}
+                        color="white"
+                        marginRight="20px"
+                      />
+                      {menu.name}
+                    </LinkOverlay>
+                  </NextLink>
+                </LinkBox>
+              </ListItem>
+            ))}
           </List>
         </Box>
       </Box>
