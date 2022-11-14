354449097f11aa13bfbd0370355df44debd6e409
Arista Indrajaya <arista.indrajaya@gmail.com>
2022-11-15 05:15:00 +0700

    Call important libs/component ui

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,6 +1,11 @@
+import { Box, List, ListItem, ListIcon, Divider, Center, LinkBox, LinkOverlay} from "@chakra-ui/layout";
+import {MdHome, MdSearch, MdLibraryMusic, MdPlaylistAdd, MdFavorite} from "react-icons/md";
+
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <Box>
+      Sidebar
+    </Box>
   )
 }
 
