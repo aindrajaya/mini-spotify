80ad9b8f23dd788b43a7fd212fcd489eecf57d77
Arista Indrajaya <arista.indrajaya@gmail.com>
2022-11-17 05:32:00 +0700

    Seed implementation - artist data

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,7 +4,26 @@ import { artistsData } from "./songsData";
 
 const prisma = new PrismaClient();
 
-const run = async () => {};
+const run = async () => {
+  await Promise.all(
+    artistsData.map(async (artist) => {
+      return prisma.artist.upsert({
+        where: { name: artist.name },
+        update: {},
+        create: {
+          name: artist.name,
+          songs: {
+            create: artist.songs.map((song) => ({
+              name: song.name,
+              duration: song.duration,
+              url: song.url,
+            })),
+          },
+        },
+      });
+    })
+  );
+};
 
 run()
   .catch((e) => {
