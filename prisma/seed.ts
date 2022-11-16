dd2f0ed5bc6a5a6fac446d119e63d948c9963628
Arista Indrajaya <arista.indrajaya@gmail.com>
2022-11-17 05:12:00 +0700

    Seed implementation

---
+++ prisma/seed.ts
@@ -0,0 +1,17 @@
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
+import { artistsData } from "./songsData";
+
+const prisma = new PrismaClient();
+
+const run = async () => {};
+
+run()
+  .catch((e) => {
+    // eslint-disable-next-line no-console
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
