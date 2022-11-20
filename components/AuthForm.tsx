f87ae8fc31e35f7bd5d609441cf7f86535521c53
Arista Indrajaya <arista.indrajaya@gmail.com>
2022-11-21 05:22:00 +0700

    init auth component

---
+++ components/AuthForm.tsx
@@ -0,0 +1,10 @@
+import { Box, Flex, Input, Button } from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import { useSWRConfig } from "swr";
+import { auth } from "../lib/mutation";
+
+const AuthForm = () => {
+
+}
+
+export default AuthForm;
