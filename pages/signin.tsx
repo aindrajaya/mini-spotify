ea1e8d722dbc1ea9215c68e74d0784a9438de622
Arista Indrajaya <arista.indrajaya@gmail.com>
2022-11-21 06:19:33 +0700

    signin/up layout

---
+++ pages/signin.tsx
@@ -0,0 +1,7 @@
+import AuthForm from "../components/AuthForm";
+
+const Signin = () => {
+  return <AuthForm mode="signin" />;
+};
+
+export default Signin;
---
+++ pages/signup.tsx
@@ -0,0 +1,7 @@
+import AuthForm from "../components/AuthForm";
+
+const Signup = () => {
+  return <AuthForm mode="signup" />;
+};
+
+export default Signup;
