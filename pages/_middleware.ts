064e54e7964d2ece169a97f4086c5cfbc7099012
Arista Indrajaya <arista.indrajaya@gmail.com>
2022-11-22 06:12:00 +0700

    implement middleware
    This features is only works on Next version 12, and you must be choose the right version to do it, because there are problems if you implemented in other version/ latest version

---
+++ pages/_middleware.ts
@@ -0,0 +1,16 @@
+import { NextResponse } from "next/server";
+
+const signedinPages = ["/", "/playlist", "/library"];
+
+export default function middleware(req: {
+  nextUrl: { pathname: string };
+  cookies: { TRAX_ACCESS_TOKEN: any };
+}) {
+  if (signedinPages.find((p) => p === req.nextUrl.pathname)) {
+    const token = req.cookies.TRAX_ACCESS_TOKEN;
+
+    if (!token) {
+      return NextResponse.redirect("/signin");
+    }
+  }
+}
