a2f1582e82f9d3d78d4527a7c282ad63d2c0f2b9
Arista Indrajaya <arista.indrajaya@gmail.com>
2022-11-23 06:21:00 +0700

    create and implement gradient layout

---
+++ components/GradientLayout.tsx
@@ -0,0 +1,42 @@
+import { Box, Flex, Text } from "@chakra-ui/layout";
+import { Image } from "@chakra-ui/react";
+
+const GradientLayout = ({
+  color,
+  children,
+  image,
+  subtitle,
+  title,
+  description,
+  roundImage,
+}) => {
+  return (
+    <Box
+      height="100%"
+      overflowY="auto"
+      bgGradient={`linear(${color}.500 0%, ${color}.600 15%, ${color}.700 40%, rgba(0,0,0,0.95) 75%)`}
+    >
+      <Flex bg={`${color}.600`} padding="40px" align="end">
+        <Box padding="20px">
+          <Image
+            boxSize="160px"
+            boxShadow="2xl"
+            src={image}
+            borderRadius={roundImage ? "100%" : "3px"}
+          />
+        </Box>
+        <Box padding="20px" lineHeight="40px" color="white">
+          <Text fontSize="sm" fontWeight="bold" casing="uppercase">
+            {subtitle}
+          </Text>
+          <Text fontSize="5xl">{title}</Text>
+          <Text fontSize="2xl" fontWeight="100">
+            {description}
+          </Text>
+        </Box>
+      </Flex>
+    </Box>
+  );
+};
+
+export default GradientLayout;
