4e2d610174840d63d7f85c994085b9b27a32ec38
Arista Indrajaya <arista.indrajaya@gmail.com>
2022-11-23 05:38:00 +0700

    custom hooks swr
    Implementation custom hooks that handle data using SWR

---
+++ lib/hooks.ts
@@ -0,0 +1,12 @@
+import useSWR from "swr";
+import fetcher from "./fetcher";
+
+export const usePlaylist = () => {
+  const { data, error } = useSWR("/playlist", fetcher);
+
+  return {
+    playlists: (data as any) || [],
+    isLoading: !data && !error,
+    isError: error,
+  };
+};
