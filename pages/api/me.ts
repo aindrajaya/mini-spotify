c7a70e4bb2d11e2aed2bf0c5c3986869be778240
Arista Indrajaya <arista.indrajaya@gmail.com>
2022-12-01 06:14:00 +0700

    update user api that show playlist count

--- pages/api/me.ts
+++ pages/api/me.ts
@@ -1,5 +1,12 @@
 import { ValidateRoute } from "../../lib/auth";
+import prisma from "../../lib/prisma";
 
-export default ValidateRoute((req: any, res: any, user: any) => {
-  res.json(user);
+export default ValidateRoute(async (req: any, res: any, user: any) => {
+  const playlistCount = await prisma.playlist.count({
+    where: {
+      userId: user.id,
+    },
+  });
+
+  res.json({ ...user, playlistCount });
 });
