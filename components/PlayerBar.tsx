94a850037b9d01bef9ee57fa37b41d8041a8cd2a
Arista Indrajaya <arista.indrajaya@gmail.com>
2022-12-05 05:49:00 +0700

    Add and update player bar

--- components/PlayerBar.tsx
+++ components/PlayerBar.tsx
@@ -1,7 +1,7 @@
 import { Box, Flex } from "@chakra-ui/layout";
 
 const PlayerBar = () => {
-  return <Box>player bar</Box>;
+  return <Box>player bar component</Box>;
 };
 
 export default PlayerBar;
--- components/PlayerLayout.tsx
+++ components/PlayerLayout.tsx
@@ -1,4 +1,5 @@
 import { Box } from "@chakra-ui/layout";
+import PlayerBar from "./PlayerBar";
 import Sidebar from "./Sidebar";
 
 const PlayerLayout = ({ children }: any) => {
@@ -11,7 +12,7 @@ const PlayerLayout = ({ children }: any) => {
         <Box height="calc(100vh - 100px)">{children}</Box>
       </Box>
       <Box position="absolute" left="0" bottom="0">
-        Player
+        <PlayerBar />
       </Box>
     </Box>
   );
