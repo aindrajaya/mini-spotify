71c9e4bcfb59b2cb27ec901f98cf452ff96736f2
Arista Indrajaya <arista.indrajaya@gmail.com>
2022-12-05 07:02:00 +0700

    implement player component to the player bar

--- components/PlayerBar.tsx
+++ components/PlayerBar.tsx
@@ -1,4 +1,5 @@
 import { Box, Flex, Text } from "@chakra-ui/layout";
+import Player from "./Player";
 
 const PlayerBar = () => {
   return (
@@ -8,7 +9,9 @@ const PlayerBar = () => {
           <Text fontSize="large">Song Name</Text>
           <Text fontSize="sm">Artist name</Text>
         </Box>
-        <Box width="40%">Controls</Box>
+        <Box width="40%">
+          <Player />
+        </Box>
       </Flex>
     </Box>
   );
