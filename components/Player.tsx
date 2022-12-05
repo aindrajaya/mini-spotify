1dd3d445516a2ea11f18625217f31dda32fe783d
Arista Indrajaya <arista.indrajaya@gmail.com>
2022-12-05 07:11:00 +0700

    Player component UI dev

--- components/Player.tsx
+++ components/Player.tsx
@@ -16,14 +16,102 @@ import {
   MdShuffle,
   MdSkipPrevious,
   MdSkipNext,
-  MdOutlinePlayCircle,
+  MdOutlinePlayCircleFilled,
   MdOutlinePauseCircleFilled,
   MdOutlineRepeat,
 } from "react-icons/md";
 import { useStoreActions } from "easy-peasy";
+import { formatTime } from "../lib/formatters";
 
-const Player = () => {
-  return <Box>hello from player</Box>;
+const Player = ({ songs, activeSong }) => {
+  return (
+    <Box>
+      <Box>
+        <ReactHowler playing={playing} src={activeSong?.url} />
+      </Box>
+      <Center color="gray.600">
+        <ButtonGroup>
+          <IconButton
+            outline="none"
+            variant="link"
+            aria-label="shuffle"
+            fontSize="24px"
+            icon={<MdShuffle />}
+          />
+          <IconButton
+            outline="none"
+            variant="link"
+            aria-label="shuffle"
+            fontSize="24px"
+            icon={<MdSkipPrevious />}
+          />
+          {playing ? (
+            <IconButton
+              outline="none"
+              variant="link"
+              aria-label="shuffle"
+              fontSize="24px"
+              icon={<MdOutlinePauseCircleFilled />}
+              onClick={() => setPlaying(true)}
+            />
+          ) : (
+            <IconButton
+              outline="none"
+              variant="link"
+              aria-label="shuffle"
+              fontSize="24px"
+              icon={<MdOutlinePlayCircleFilled />}
+              onClick={() => setPlaying(false)}
+            />
+          )}
+          <IconButton
+            outline="none"
+            variant="link"
+            aria-label="shuffle"
+            fontSize="24px"
+            icon={<MdOutlinePauseCircleFilled />}
+          />
+          <IconButton
+            outline="none"
+            variant="link"
+            aria-label="shuffle"
+            fontSize="24px"
+            icon={<MdSkipNext />}
+          />
+          <IconButton
+            outline="none"
+            variant="link"
+            aria-label="shuffle"
+            fontSize="24px"
+            icon={<MdOutlineRepeat />}
+          />
+        </ButtonGroup>
+      </Center>
+      <Box color="gray.600">
+        <Flex justify="center" align="center">
+          <Box width="10%">
+            <Text>0.0</Text>
+          </Box>
+          <Box width="80%">
+            <RangeSlider
+              aria-label={["min", "max"]}
+              step={0.1}
+              min={0}
+              id="player-range"
+            >
+              <RangeSliderTrack>
+                <RangeSliderFilledTrack />
+              </RangeSliderTrack>
+              <RangeSliderThumb index={0} />
+            </RangeSlider>
+          </Box>
+          <Box width="10%" textAlign="right">
+            <Text fontSize="xs">Duration</Text>
+          </Box>
+        </Flex>
+      </Box>
+    </Box>
+  );
 };
 
 export default Player;
