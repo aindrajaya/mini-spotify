a113bc292cb533543f0ed9098bd5dace8c02ff9e
Arista Indrajaya <arista.indrajaya@gmail.com>
2023-01-01 06:23:00 +0700

    fix mutation and fetcher

--- lib/fetcher.ts
+++ lib/fetcher.ts
@@ -1,11 +1,11 @@
-export default async function fetcher(url: string, data = undefined) {
+export default async function fetcher(url: string, data?: any) {
   const res = await fetch(`${window.location.origin}/api${url}`, {
     method: data ? "POST" : "GET",
     credentials: "include",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify(data),
+    body: data ? JSON.stringify(data) : undefined,
   });
   if (res.status > 299 && res.status < 200) {
     throw new Error();
