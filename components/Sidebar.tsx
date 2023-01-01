d573a5041ef038c085ae8b46eb00c77a7c25bb4a
Arista Indrajaya <arista.indrajaya@gmail.com>
2023-01-01 07:47:00 +0700

    fix unused type and add type

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -6,7 +6,6 @@ import {
   ListItem,
   ListIcon,
   Divider,
-  Center,
   LinkBox,
   LinkOverlay,
 } from "@chakra-ui/layout";
@@ -17,8 +16,8 @@ import {
   MdPlaylistAdd,
   MdFavorite,
 } from "react-icons/md";
-import { Key, ReactChild, ReactFragment, ReactPortal } from "react";
 import { usePlaylist } from "../lib/hooks";
+import { Playlist } from "./types/Playlist";
 
 const NavMenu = [
   {
@@ -111,7 +110,7 @@ const Sidebar = () => {
         <Divider marginY="10px" color="gray.800" />
         <Box height="66%" overflowY="auto" paddingY="20px">
           <List>
-            {playlists.map((playlist) => (
+            {playlists.map((playlist: Playlist) => (
               <ListItem paddingX="20px" key={playlist.id}>
                 <LinkBox>
                   <NextLink
